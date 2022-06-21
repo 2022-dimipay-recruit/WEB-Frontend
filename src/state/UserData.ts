6d4f4a46ddefc6be30fbcd81c91d299cb98323f3
오명훈 <omh02033@gmail.com>
2022-06-21 18:04:40 +0900

    chore

--- src/state/UserData.ts
+++ src/state/UserData.ts
@@ -1,11 +1,11 @@
-import { api } from '@/api';
+import { fetchUserData } from '@/api';
 import { selectorFamily } from 'recoil';
 
 export default selectorFamily({
   key: 'fetchUserData',
   get: (username) => async () => {
     try {
-      return await api<'userInfo'>('GET', `/user?name=${username as string}`);
+      return await fetchUserData(username as string);
     } catch {
       return undefined;
     }
